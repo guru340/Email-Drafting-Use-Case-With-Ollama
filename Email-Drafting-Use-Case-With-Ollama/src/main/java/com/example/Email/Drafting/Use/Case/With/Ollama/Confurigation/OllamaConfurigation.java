cae603ac46ed4532df7e17fc5a9382a9adbe31f7
guru340 <gurusangwani06@gmail.com>
2026-09-17 19:45:26 +0530

    Upgrading the Controller

--- Email-Drafting-Use-Case-With-Ollama/src/main/java/com/example/Email/Drafting/Use/Case/With/Ollama/Confurigation/OllamaConfurigation.java
+++ Email-Drafting-Use-Case-With-Ollama/src/main/java/com/example/Email/Drafting/Use/Case/With/Ollama/Confurigation/OllamaConfurigation.java
@@ -3,7 +3,9 @@ package com.example.Email.Drafting.Use.Case.With.Ollama.Confurigation;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class OllamaConfurigation {
 
     @Bean("ollamaChatClient")
--- Email-Drafting-Use-Case-With-Ollama/src/main/java/com/example/Email/Drafting/Use/Case/With/Ollama/Controller/OllamaController.java
+++ Email-Drafting-Use-Case-With-Ollama/src/main/java/com/example/Email/Drafting/Use/Case/With/Ollama/Controller/OllamaController.java
@@ -1,6 +1,7 @@
 package com.example.Email.Drafting.Use.Case.With.Ollama.Controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class OllamaController {
     private final  ChatClient chatClient;
 
 
-    public OllamaController(ChatClient chatClient) {
+    public OllamaController(@Qualifier("ollamaChatClient") ChatClient chatClient) {
         this.chatClient = chatClient;
     }
 
